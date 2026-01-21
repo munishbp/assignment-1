b0416aebbd624a645530cac415c3fc9444930012
Munish Persaud <146845753+munishbp@users.noreply.github.com>
2026-01-20 21:04:42 -0500

    import new library and modify some types

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,13 +1,13 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import {Request,Response} from "express";
 
-export default (req, res) => {
-  const  num:string  = req.params;
-  //console.log(req.params);
+export default (req:Request, res:Response) => {
+  const num  = req.params.num;
 
   const fibN:number = fibonacci(parseInt(num));
-  let result:string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
